dfd2206394babc40f899cbcb1c2897faa82beb2d
Sohel S <sohelsd@gmail.com>
2012-03-06 18:55:02 -0500

    first commit

---
+++ Adventeur/Adventeur/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Adventeur
+//
+//  Created by Sohel Siddique on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
