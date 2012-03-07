20edfa7386789ffba486e6d95e934222a2ce3d36
Sohel S <sohelsd@gmail.com>
2012-03-06 21:38:26 -0500

    first commit 2

---
+++ AdventeurApp/AdventeurApp/CheckinViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckinViewController.h
+//  AdventeurApp
+//
+//  Created by Sohel Siddique on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckinViewController : UIViewController
+
+@end
---
+++ AdventeurApp/AdventeurApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AdventeurApp
+//
+//  Created by Sohel Siddique on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
